7bce1065a3ed2b4379fd2a2dcbb92a50559ffc55
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2020-08-05 14:59:06 +0300

    Add user controller

---
+++ AnimalShelterAPI/AnimalShelterAPI/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AnimalShelterAPI.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using System.Threading.Tasks;
+
+namespace AnimalShelterAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _config;
+
+        public UsersController(
+            IConfiguration config,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager
+            )
+        {
+            _config = config;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public IActionResult Token(UserDTO userInfo)
+        {
+            return Ok(GenerateJWTToken(userInfo));
+        }
+
+
+        string GenerateJWTToken(UserDTO userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTSecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+            var token = new JwtSecurityToken(
+            issuer: _config["JWTIssuer"],
+            audience: _config["JWTAudience"],
+            claims: claims,
+            expires: DateTime.Now.AddDays(30),
+            signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
