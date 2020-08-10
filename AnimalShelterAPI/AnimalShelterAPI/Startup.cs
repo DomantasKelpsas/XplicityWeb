9a26b625f18727d44e30ef24ab7f512501605f31
Justas <jradkevicius@gmail.com>
2020-08-10 10:59:49 +0300

    Add JWT key, remove sqllite reference

--- AnimalShelterAPI/AnimalShelterAPI/Startup.cs
+++ AnimalShelterAPI/AnimalShelterAPI/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using AnimalShelterAPI.Auth;
