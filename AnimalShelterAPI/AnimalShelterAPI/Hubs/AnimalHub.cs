53d814c234b6b1a1bc948aa9032c482e62adcf21
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2020-08-11 11:18:06 +0300

    Add Animal hub

---
+++ AnimalShelterAPI/AnimalShelterAPI/Hubs/AnimalHub.cs
@@ -0,0 +1,14 @@
+﻿using AnimalShelterAPI.Models.DTO;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace AnimalShelterAPI.Hubs
+{
+    public class AnimalHub : Hub
+    {
+        public async Task SendCreatedProduct(AnimalDto animal)
+        {
+            await Clients.Others.SendAsync("ReceiveCreatedAnimal", animal);
+        }
+    }
+}
