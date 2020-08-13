45ba1fd8fbb3eb9c53d5d35b6aeb758eacbb2e8d
Justas <jradkevicius@gmail.com>
2020-08-13 09:45:28 +0300

    Mapping for edit dto

--- AnimalShelterAPI/AnimalShelterAPI/Configuration/AutoMapperConfiguration.cs
+++ AnimalShelterAPI/AnimalShelterAPI/Configuration/AutoMapperConfiguration.cs
@@ -21,6 +21,10 @@ namespace AnimalShelterAPI.Configuration
                 .ForMember(dest => dest.Years, opt => opt.MapFrom(src => src.Birthday == null ? 0 : (DateTime.Now - src.Birthday.Value).Days / 365 ))
                 .ForMember(dest => dest.Months, opt => opt.MapFrom(src => src.Birthday == null ? 0 : ((DateTime.Now - src.Birthday.Value).Days % 365) / 30));
             CreateMap<NewAnimalDto, Animal>().ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => DateTime.Now.AddYears(-src.Years).AddMonths(-src.Months)));
+            CreateMap<Animal, EditAnimalDto>()
+                .ForMember(dest => dest.Years, opt => opt.MapFrom(src => src.Birthday == null ? 0 : (DateTime.Now - src.Birthday.Value).Days / 365))
+                .ForMember(dest => dest.Months, opt => opt.MapFrom(src => src.Birthday == null ? 0 : ((DateTime.Now - src.Birthday.Value).Days % 365) / 30));
+            CreateMap<EditAnimalDto, Animal>().ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => DateTime.Now.AddYears(-src.Years).AddMonths(-src.Months)));
         }
     }
 }
